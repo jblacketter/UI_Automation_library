e3bc4a099bd40fb4dc349d18e4bb32ac082444d9
Jack Blacketter <jack.blacketter@puppetlabs.com>
2014-05-28 15:40:17 -0700

    refactored higgs test examples to use higgs page objects to complete forms.

--- page_elements/higgs.rb
+++ page_elements/higgs.rb
@@ -34,10 +34,18 @@ module Higgs
 ### TODO: rework this to use json or xml data
 ##############################################################################################
   def form_fill_machine_information
-    @higgs.enter_text('q_puppetmaster_certname',"#{@puppetmaster_certname}")
-    @higgs.enter_text('q_puppetmaster_dnsaltnames',"#{@puppetmaster_dnsaltnames}")
-    @higgs.enter_text('q_puppetdb_hostname',"#{@puppetdb_hostname}")
-    @higgs.enter_text('q_puppetmaster_enterpriseconsole_hostname',"#{@puppetmaster_enterpriseconsole_hostname}")
+      @higgs.page_title_present?('Puppet Enterprise Installer')
+      @higgs.link_present?('email the CEO.')
+      @higgs.select_link("Let's get started")
+      @higgs.page_title_present?('Puppet Enterprise Installer - Choose your deployment')
+      @higgs.select_id('combined-description')
+      @higgs.enter_text('master_hostname', @hostname)
+      @higgs.enter_text('master_alt_names', 'localhost')
+      @higgs.enter_text('master_ssh_username','root')
+      @higgs.enter_text('master_ssh_key_file', '/root/.ssh/id_rsa-acceptance')
+      @higgs.enter_text('console_username','consolesuperuser@email.com')
+      @higgs.enter_text('console_password','Puppet11')
+      @higgs.enter_text('smtp_hostname',@hostname)
   end
 
   def form_fill_database_information
@@ -56,14 +64,11 @@ module Higgs
     @higgs.enter_text('q_puppet_enterpriseconsole_auth_password_confirm',"#{@yaml_data['console_information']['q_puppet_enterpriseconsole_auth_password_confirm']}")
   end
 
-  def form_fill_smtp_info
-    @higgs.enter_text('q_puppet_enterpriseconsole_smtp_host','localhost')
-    @higgs.enter_text('q_puppet_enterpriseconsole_smtp_port','25')
-    @higgs.select_id('q_puppet_enterpriseconsole_smtp_use_tls')
-    @higgs.select_id('q_puppet_enterpriseconsole_smtp_user_auth')
-    @higgs.enter_text('q_puppet_enterpriseconsole_smtp_username','console')
-    @higgs.enter_text('q_puppet_enterpriseconsole_smtp_password','Puppet11')
-    @higgs.enter_text('q_puppet_enterpriseconsole_smtp_password_confirm','Puppet11')
+  def form_fill_more_options
+    @higgs.select_id('smtp_more_options')
+    @higgs.enter_text('smtp_port','25')
+    @higgs.enter_text('smtp_username','smtpUsername')
+    @higgs.enter_text('smtp_password','Puppet11')
   end
 
 end
--- rspec_higgs/spec/higgs_form_combined_install.rb
+++ rspec_higgs/spec/higgs_form_combined_install.rb
@@ -16,66 +16,28 @@ require 'open-uri'
 
     after(:all) do
       write_test_log('Ending test run')
-      @higgs.teardown
+      # @higgs.teardown
     end
 
-    describe 'Select Combined install' do
+    describe 'Create Combined install' do
 
       it 'Enter form We need to know a few things' do
         write_test_log('Fill combined install question form')
-        @higgs.page_title_present?('Puppet Enterprise Installer')
-        @higgs.link_present?('email the CEO.')
-        @higgs.select_link("Let's get started")
-        @higgs.page_title_present?('Puppet Enterprise Installer - Choose your deployment')
-        @higgs.select_id('combined-description')
-        @higgs.enter_text('master_hostname', @hostname)
-        @higgs.enter_text('master_alt_names', 'localhost')
-        @higgs.enter_text('master_ssh_username','root')
-        # @higgs.enter_text('master_sudo_password', 'Puppet11')
-        @higgs.enter_text('master_ssh_key_file', '/root/.ssh/id_rsa-acceptance')
-        # @higgs.enter_text('master_ssh_key_passphrase', 'Puppet1')
-        @higgs.enter_text('console_username','consolesuperuser@email.com')
-        @higgs.enter_text('console_password','Puppet11')
-        @higgs.enter_text('smtp_hostname',@hostname)
-      end
-
-      # # it 'enter form database install' do
-      # #   @higgs.select_css("label > label") # <-- select radio button 'Use an existing PostgreSQL instance'
-      # #   @higgs.enter_text('database_hostname', 'postgresql_server_dns_name')
-      # #   @higgs.enter_text('database_port','8888')
-      # #   @higgs.enter_text('puppetdb_database_username','pe-puppetdb_user')
-      # #   @higgs.enter_text('puppetdb_database_password','Puppet11')
-      # #   @higgs.enter_text('console_database_name','console_database_name')
-      # #   @higgs.enter_text('console_database_username', 'console_database_user')
-      # #   @higgs.enter_text('console_database_password','console_database_password')
-      # #   @higgs.enter_text('console_auth_db_name','console_auth_database_name')
-      # #   @higgs.enter_text('console_auth_db_username','console_authdb_user')
-      # #   @higgs.enter_text('console_auth_db_password','console_authdb_password')
-      # # end
-
-      it 'Enter form more options' do
-        @higgs.select_id('smtp_more_options')
-        @higgs.enter_text('smtp_port','25')
-        @higgs.enter_text('smtp_username','smtpUsername')
-        @higgs.enter_text('smtp_password','Puppet11')
-      end
-
-      it 'Submit' do
+        form_fill_machine_information
+        form_fill_more_options
         @higgs.select_css('input.btn.btn-default')
       end
 
-      it 'verify answer file on host' do
+      it 'Verify answer file on host' do
         get_answers_file_blob
       end
 
-      it 'Confirm' do
-        # if verification passed continue
+      it 'Confirm the plan' do
         @higgs.page_title_present?('Puppet Enterprise Installer - Confirm the plan')
         @higgs.select_link('Continue')
       end
 
       it 'Deploy Now' do
-        # if verifications pass then deploy
         @higgs.select_id('deploy-btn')
       end
 
