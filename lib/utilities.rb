a79dfaee905ecb9c873d76fd12d6252cac5b394b
Jack Blacketter <jack.blacketter@puppetlabs.com>
2014-05-29 09:44:19 -0700

    Removed outdated example files in archive and data

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -5,9 +5,9 @@ module Utilities
 
   def get_answers_file_blob
     # TODO this will cat out the answers file from host. Need to parse and match for expected elements.
-    Net::SSH.start('r7ewsv5ylwrbr6k', 'root') do |ssh|
-       remote_data = open("https://r7ewsv5ylwrbr6k.delivery.puppetlabs.net:3000/answers/r7ewsv5ylwrbr6k.answers").read
-      ssh.exec("cd /opt/puppet/share/installer/answers; ls; cat r7ewsv5ylwrbr6k.answers")
+    Net::SSH.start('obtvi5oe6t3cepz', 'root') do |ssh|
+       remote_data = open("https://obtvi5oe6t3cepz.delivery.puppetlabs.net:3000/answers/r7ewsv5ylwrbr6k.answers").read
+      ssh.exec("cd /opt/puppet/share/installer/answers; ls; cat obtvi5oe6t3cepz.answers")
       end
     end
 
